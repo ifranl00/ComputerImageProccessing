4a02a56a8e440ea229fa905d6135fd8fae70c80d
Irene Francisco López <36732712+ifranl00@users.noreply.github.com>
2021-05-31 06:00:53 -0700

    All working

--- p5/LAB5_Irene_Francisco_Lopez.m
+++ p5/LAB5_Irene_Francisco_Lopez.m
@@ -47,78 +47,113 @@
 % should be able to switch between colormaps using the drop-down menu in the bottom left corner. 
 % Use the following colormaps: autumn, bone, cool, copper, hot, hsv, jet, pink, prism, spring, 
 % summer, winter.
-% I1=imread('portrait.jpg');
-% figure(1)
-% 
-% subplot(1,2,1),imshow(I1);
-% subplot(1,2,2),rgbplot(autumn),axis([0 256 0 1]),grid,colormap(autumn),colorbar ('horiz'),title('Plot of autumn colormap','FontSize',15);
-% 
-% uicontrol(1,'Style', 'popupmenu',... % 1 for handling the figure and it will pop up a list of choices
-%             'String', 'autumn|bone|cool|copper|hot|hsv|jet|pink|prism|spring|summer|winter',...
-%             'Position', [20 1 100 50],... % where is the control
-%             'Callback', @setmap2); % call function setmap when interacted with the control
-% function setmap2(h,event)
-%     val = get(h,'Value');
-%     switch val
-%         case 1
-%             subplot(1,2,2),rgbplot(autumn),axis([0 256 0 1]),grid,colormap(autumn),colorbar ('horiz'),title('Plot of autumn colormap','FontSize',15);
-%         case 2
-%             subplot(1,2,2),rgbplot(bone),axis([0 256 0 1]),grid,colormap(bone),colorbar ('horiz'),title('Plot of bone colormap','FontSize',15);
-%         case 3
-%             subplot(1,2,2),rgbplot(cool),axis([0 256 0 1]),grid,colormap(cool),colorbar ('horiz'),title('Plot of cool colormap','FontSize',15);
-%         case 4
-%             subplot(1,2,2),rgbplot(copper),axis([0 256 0 1]),grid,colormap(copper),colorbar ('horiz'),title('Plot of copper colormap','FontSize',15);
-%         case 5
-%             subplot(1,2,2),rgbplot(hot),axis([0 256 0 1]),grid,colormap(hot),colorbar ('horiz'),title('Plot of hot colormap','FontSize',15);
-%         case 6
-%             subplot(1,2,2),rgbplot(hsv),axis([0 256 0 1]),grid,colormap(hsv),colorbar ('horiz'),title('Plot of hsv colormap','FontSize',15);
-%         case 7
-%             subplot(1,2,2),rgbplot(jet),axis([0 256 0 1]),grid,colormap(jet),colorbar ('horiz'),title('Plot of jet colormap','FontSize',15);
-%         case 8
-%             subplot(1,2,2),rgbplot(pink),axis([0 256 0 1]),grid,colormap(pink),colorbar ('horiz'),title('Plot of pink colormap','FontSize',15);
-%         case 9
-%             subplot(1,2,2),rgbplot(prism),axis([0 256 0 1]),grid,colormap(prism),colorbar ('horiz'),title('Plot of prism colormap','FontSize',15);
-%         case 10
-%             subplot(1,2,2),rgbplot(spring),axis([0 256 0 1]),grid,colormap(spring),colorbar ('horiz'),title('Plot of spring colormap','FontSize',15);
-%         case 11
-%             subplot(1,2,2),rgbplot(summer),axis([0 256 0 1]),grid,colormap(summer),colorbar ('horiz'),title('Plot of summer colormap','FontSize',15);
-%         case 12
-%             subplot(1,2,2),rgbplot(winter),axis([0 256 0 1]),grid,colormap(winter),colorbar ('horiz'),title('Plot of winter colormap','FontSize',15);
-%     end
-% end
+I1=imread('portrait.jpg');
+figure(1)
+
+subplot(1,2,1),imshow(I1);
+subplot(1,2,2),rgbplot(autumn),axis([0 256 0 1]),grid,colormap(autumn),colorbar ('horiz'),title('Plot of autumn colormap','FontSize',15);
+
+uicontrol(1,'Style', 'popupmenu',... % 1 for handling the figure and it will pop up a list of choices
+            'String', 'autumn|bone|cool|copper|hot|hsv|jet|pink|prism|spring|summer|winter',...
+            'Position', [20 1 100 50],... % where is the control
+            'Callback', @setmap2); % call function setmap when interacted with the control
+function setmap2(h,event)
+    val = get(h,'Value');
+    switch val
+        case 1
+            subplot(1,2,2),rgbplot(autumn),axis([0 256 0 1]),grid,colormap(autumn),colorbar ('horiz'),title('Plot of autumn colormap','FontSize',15);
+        case 2
+            subplot(1,2,2),rgbplot(bone),axis([0 256 0 1]),grid,colormap(bone),colorbar ('horiz'),title('Plot of bone colormap','FontSize',15);
+        case 3
+            subplot(1,2,2),rgbplot(cool),axis([0 256 0 1]),grid,colormap(cool),colorbar ('horiz'),title('Plot of cool colormap','FontSize',15);
+        case 4
+            subplot(1,2,2),rgbplot(copper),axis([0 256 0 1]),grid,colormap(copper),colorbar ('horiz'),title('Plot of copper colormap','FontSize',15);
+        case 5
+            subplot(1,2,2),rgbplot(hot),axis([0 256 0 1]),grid,colormap(hot),colorbar ('horiz'),title('Plot of hot colormap','FontSize',15);
+        case 6
+            subplot(1,2,2),rgbplot(hsv),axis([0 256 0 1]),grid,colormap(hsv),colorbar ('horiz'),title('Plot of hsv colormap','FontSize',15);
+        case 7
+            subplot(1,2,2),rgbplot(jet),axis([0 256 0 1]),grid,colormap(jet),colorbar ('horiz'),title('Plot of jet colormap','FontSize',15);
+        case 8
+            subplot(1,2,2),rgbplot(pink),axis([0 256 0 1]),grid,colormap(pink),colorbar ('horiz'),title('Plot of pink colormap','FontSize',15);
+        case 9
+            subplot(1,2,2),rgbplot(prism),axis([0 256 0 1]),grid,colormap(prism),colorbar ('horiz'),title('Plot of prism colormap','FontSize',15);
+        case 10
+            subplot(1,2,2),rgbplot(spring),axis([0 256 0 1]),grid,colormap(spring),colorbar ('horiz'),title('Plot of spring colormap','FontSize',15);
+        case 11
+            subplot(1,2,2),rgbplot(summer),axis([0 256 0 1]),grid,colormap(summer),colorbar ('horiz'),title('Plot of summer colormap','FontSize',15);
+        case 12
+            subplot(1,2,2),rgbplot(winter),axis([0 256 0 1]),grid,colormap(winter),colorbar ('horiz'),title('Plot of winter colormap','FontSize',15);
+    end
+end
 
 %% EXERCISE 2.A color map is a matrix consisting of three columns, each containing 256 elements within the 
 % range [0,1]. Create your own colormap, display its plot, and impose it on the portrait.jpg
-% I2=imread('portrait.jpg'); 
-% x = (1:128)'/128;
-% y = (1:64)'/64;
-% z = (1:256)'/256;
-% r = [x; flipud(x)];
-% g = z;
-% b = z;
-% map = [r g b];
-% figure;
-% imshow(I2);
-% colormap(map);
-% figure;
-% rgbplot(map);
-% colorbar('horiz');
-% colormap(map)
+I2=imread('portrait.jpg'); 
+x = (1:128)'/128;
+y = (1:64)'/64;
+z = (1:256)'/256;
+r = [x; flipud(x)];
+g = z;
+b = z;
+map = [r g b];
+figure;
+imshow(I2);
+colormap(map);
+figure;
+rgbplot(map);
+colorbar('horiz');
+colormap(map)
 
 %% EXAMPLE 4. Customizing colormap of the current figure
 % L1=imread('portrait.jpg'); 
 % imshow(L1);
 % colormapeditor % double click to change color
 
-%% EXERCISE 3. With the use of colormap editor, try to create your own colormap in the way that the teeth can be 
-% distinguished in the face.jpg image. You can use the imtool tool to accurately determine pixel 
-% values.
+%% EXAMPLE 5. Image in the indexed color and RGB color space.
+% L1=randi(256,3);
+% figure 
+% imshow (L1, hsv(256), 'InitialMagnification','fit')
+% title('Image in the indexed color');
+% L2 = ind2rgb (L1, hsv); % converts the indexed image X and corresponding colormap hsv to RGB (truecolor) format.
+% figure 
+% imshow (L2, 'InitialMagnification','fit')
+% title('Image in the RGB color space');
+% figure 
+% imshow (L2(:,:,1), 'InitialMagnification','fit')
+% title('Contribution of RED channel');
+% figure 
+% imshow (L2(:,:,2), 'InitialMagnification','fit')
+% title('Contribution of GREEN channel');
+% figure 
+% imshow (L2(:,:,3), 'InitialMagnification','fit')
+% title('Contribution of BLUE channel');
 
-I3 = imread('portrait.jpg');
-[I,mymap]=imread('portrait.jpg');
+%% EXERCISE 4. Create two images, size 2 x 2 pixels in RGB color space, and the given layout.
+
+W=zeros(2,2,3); %matrix of zeros with dimensions 2x2x3
+% Equal to 0-1 (row,column,color) where color are 1=red, 2=green, 3=blue or :
+W(1,1,2)=1; % green cell
+W(1,2,3)=1; % blue cell
+W(2,1,:)=0.4; % grey cell
+W(2,2,1)=1; % red cell
 figure
-imshow(I3,mymap);
-colorbar;
-imtool(I3,mymap) % display indexed image
-colormapeditor % double click to change color
+imshow(W,'InitialMagnification','fit');
 
+W2=zeros(2,2,3);%matrix of zeros with dimensions 2x2x3
+% Equal to 0-1 (row,column,color) where color are 1=red, 2=green, 3=blue or :
+% mix red and green to obtain yellow
+    W2(1,1,1)=1; % red cell
+    W2(1,1,2)=1; % green cell
+% mix red and blue to obtain magenta
+    W2(1,2,1)=1; % red cell
+    W2(1,2,3)=1; % blue cell
+
+% equal to 0 to reach black
+    W2(2,1,:)=0; % black cell
+
+% mix green with blue to get cyan
+    W2(2,2,2)=1; % green cell
+    W2(2,2,3)=1; % blue cell
+figure
+imshow(W2,'InitialMagnification','fit');
